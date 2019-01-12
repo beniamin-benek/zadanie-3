ed666adec64d802b186437db7ec15f77396f029f
benek <ben.lyzn@wp.pl>
2019-01-12 20:25:08 +0100

    Odwołanie za pomocą dwóch kropek.

--- src/Main.java
+++ src/Main.java
@@ -13,28 +13,25 @@ class Main {
         else
             czyZawieraAlkohol = "brak";
 
-        Ingredient ingredient1 = new Ingredient();
-        ingredient1.ingredientName = "woda";
-        ingredient1.amount = 225;
-        drink1.ingredient1 = ingredient1;
+        drink1.ingredient1 = new Ingredient();
+        drink1.ingredient1.ingredientName = "woda";
+        drink1.ingredient1.amount = 225;
 
-        Ingredient ingredient2 = new Ingredient();
-        ingredient2.ingredientName = "herbata";
-        ingredient2.amount = 10;
-        drink1.ingredient2 = ingredient2;
+        drink1.ingredient2 = new Ingredient();
+        drink1.ingredient2.ingredientName = "herbata";
+        drink1.ingredient2.amount = 10;
 
-        Ingredient ingredient3 = new Ingredient();
-        ingredient3.ingredientName = "cukier";
-        ingredient3.amount = 5;
-        drink1.ingredient3 = ingredient3;
+        drink1.ingredient3 = new Ingredient();
+        drink1.ingredient3.ingredientName = "cukier";
+        drink1.ingredient3.amount = 5;
 
         System.out.println("INFORMACJE O NAPOJU:");
-        System.out.printf("Nazwa: %s\nCena: %d\nZawartość alkoholu: %s\n", drink1.drinkName, drink1.price, czyZawieraAlkohol);
-        System.out.printf("Składniki:\n- %s, %d ml\n", ingredient1.ingredientName, ingredient1.amount);
-        System.out.printf("- %s, %d ml\n", ingredient2.ingredientName, ingredient2.amount);
-        System.out.printf("- %s, %d ml\n", ingredient3.ingredientName, ingredient3.amount);
+        System.out.printf("Nazwa: %s\nCena: %d zł\nZawartość alkoholu: %s\n", drink1.drinkName, drink1.price, czyZawieraAlkohol);
+        System.out.printf("Składniki:\n- %s, %d ml\n",  drink1.ingredient1.ingredientName, drink1.ingredient1.amount);
+        System.out.printf("- %s, %d ml\n", drink1.ingredient2.ingredientName, drink1.ingredient2.amount);
+        System.out.printf("- %s, %d ml\n", drink1.ingredient3.ingredientName, drink1.ingredient3.amount);
 
-        int capacity = ingredient1.amount + ingredient2.amount + ingredient3.amount;
+        int capacity = drink1.ingredient1.amount + drink1.ingredient2.amount + drink1.ingredient3.amount;
 
         System.out.println("Pojemność drinka: " + capacity + " ml.");
 
