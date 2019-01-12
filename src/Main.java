ba4eaa1a314083b4279970212a591e4ad8ea0a65
benek <ben.lyzn@wp.pl>
2019-01-12 20:27:21 +0100

    Usunięcie spacji.

--- src/Main.java
+++ src/Main.java
@@ -27,7 +27,7 @@ class Main {
 
         System.out.println("INFORMACJE O NAPOJU:");
         System.out.printf("Nazwa: %s\nCena: %d zł\nZawartość alkoholu: %s\n", drink1.drinkName, drink1.price, czyZawieraAlkohol);
-        System.out.printf("Składniki:\n- %s, %d ml\n",  drink1.ingredient1.ingredientName, drink1.ingredient1.amount);
+        System.out.printf("Składniki:\n- %s, %d ml\n", drink1.ingredient1.ingredientName, drink1.ingredient1.amount);
         System.out.printf("- %s, %d ml\n", drink1.ingredient2.ingredientName, drink1.ingredient2.amount);
         System.out.printf("- %s, %d ml\n", drink1.ingredient3.ingredientName, drink1.ingredient3.amount);
 
